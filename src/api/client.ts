890b73eec7067b126daad78a9aed9bcf64b51766
Laimuslime <yshi03395@gmail.com>
2026-03-26 20:14:01 +0800

    Update client.ts

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://wikit.backroomswiki.top";
+export const BACKEND_URL = "https://wikit.brcnwiki.com/";
 
 export const BROWSER_HEADERS = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
